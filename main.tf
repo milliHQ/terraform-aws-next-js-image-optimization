efadaa98abc06881dc9130ead2c4c7bb5cb64096
Felix Haus <ofhouse@users.noreply.github.com>
2021-08-27 17:24:47 +0200

    Provide module path to npm-download submodule (#60)

--- main.tf
+++ main.tf
@@ -3,12 +3,13 @@
 ###############
 module "lambda_content" {
   source  = "dealmore/download/npm"
-  version = "1.0.0"
+  version = "1.1.0"
 
   module_name    = "@dealmore/tf-next-image-optimization"
   module_version = var.next_image_version
   path_to_file   = "dist.zip"
   use_local      = var.debug_use_local_packages
+  local_cwd      = path.module
 }
 
 resource "random_id" "function_name" {
