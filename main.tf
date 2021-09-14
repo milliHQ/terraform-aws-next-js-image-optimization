e7a943733c38e743685c14c19e4607bcff452d64
Wonnie Sim <wsim@plaid.com>
2021-09-14 12:17:22 -0400

    bump terraform-aws-lambda version to 2.17.0

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "random_id" "function_name" {
 
 module "image_optimizer" {
   source  = "terraform-aws-modules/lambda/aws"
-  version = "2.4.0"
+  version = "2.17.0"
 
   function_name = random_id.function_name.hex
   description   = var.deployment_name
